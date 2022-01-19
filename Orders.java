28cd242c92bc7de0e5ac391f65bb98add9f8b24a
Jack Sawyer <jack.h.sawyer@pm.me>
2022-01-19 11:31:28 -0600

    first commit

---
+++ Orders.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Orders {
+    
+    public String name;
+    public double total;
+    public boolean ready;
+
+    public ArrayList<Items> items = new ArrayList<Items>();
+}
